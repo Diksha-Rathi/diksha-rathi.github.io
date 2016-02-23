c1fe7d3a4e30f32a7d40e60b75bbe2f219a4a9ee
Diksha <diksha.2895@gmail.com>
2016-02-23 20:56:26 +0530

    changes in about-me

--- _site/js/main.js
+++ _site/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  /*if (window.location.pathname !== 'https://diksha-rathi.github.io/' && window.location.pathname !== 'https://diksha-rathi.github.io/index.html') {
+  /*if (window.location.pathname !== '/jekyll-uno/' && window.location.pathname !== '/jekyll-uno/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }*/
 
